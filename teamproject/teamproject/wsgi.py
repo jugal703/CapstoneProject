df01609fa2095e3abf97840c7848a81f69481973
txzolbayar <40209641+txzolbayar@users.noreply.github.com>
2020-04-19 23:28:59 -0500

    hi

--- teamproject/teamproject/wsgi.py
+++ teamproject/teamproject/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teamproject.settings')
+
+path = '/home/tzolba/CapstoneProject'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'teamproject.settings'
+
 
 application = get_wsgi_application()
