359d246e4fe993f5b7d9ed192553f42022eb9360
zxlin2131 <zxlin2131@users.noreply.github.com>
2020-04-23 11:16:31 -0500

    Update views.py

--- teamproject/ADTAA/views.py
+++ teamproject/ADTAA/views.py
@@ -13,11 +13,13 @@ import ADTAA.models as ADTAA_models
 import ADTAA.forms as ADTAA_forms
 from ADTAA.globals import raise_unexpected_error
 
+
 # class for the login page (landing page) of the app
 class Index(View):
     # function to get and return the html of this login page
     def get(self, request, *args, **kwargs):
         return render(request, 'ADTAA/index.html')
+
     # function to post the email and password entered by the user to check within the database
     def post(self, request, *args, **kwargs):
         email = request.POST.get('email', None)
@@ -72,6 +74,7 @@ class Index(View):
             }
             return render(request, 'ADTAA/index.html', context=context)
 
+
 # function decorator to require user's user type to be scheduler for access, redirect and disallow if not
 def scheduler_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='/ADTAA/'):
     actual_decorator = user_passes_test(
@@ -82,6 +85,7 @@ def scheduler_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, l
     if function:
         return actual_decorator(function)
     return actual_decorator
+
 
 # function decorator to require user's user type to be admin for access, redirect and disallow if not
 def admin_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='/ADTAA/'):
@@ -94,6 +98,7 @@ def admin_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login
         return actual_decorator(function)
     return actual_decorator
 
+
 # function decorator to require user's user type to be root for access, redirect and disallow if not
 def root_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='/ADTAA/'):
     actual_decorator = user_passes_test(
@@ -105,6 +110,7 @@ def root_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_
         return actual_decorator(function)
     return actual_decorator
 
+
 # function decorator to require user's user type to be admin or root for access, redirect and disallow if not
 def admin_root_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='/ADTAA/'):
     actual_decorator = user_passes_test(
@@ -115,6 +121,7 @@ def admin_root_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, 
     if function:
         return actual_decorator(function)
     return actual_decorator
+
 
 # function to return the homepage of root user to user that is logged in and has user type as root
 @login_required
@@ -243,23 +250,30 @@ def edit_solutions(request):
     return render(request, 'ADTAA/editSolutions.html', context)
 
 
-def make_solution(classes, instructors_load, rest_classes, curr_solution):
+def make_solution(classes, instructors_load, rest_of_classes, curr_solution):
     for i, elem in enumerate(classes):
+        # break when there is no instructor in instructors_load
         if len(instructors_load) == 0:
             break
-        if len(rest_classes) == 0:
+        # break when there is no class in rest_of_classes
+        if len(rest_of_classes) == 0:
             break
+
+        # get current class' time
         curr_class = ADTAA_models.Class.objects.get(course_number=classes[i][0])
         class_start_time = curr_class.start_time
         class_end_time = curr_class.end_time
         class_day = curr_class.meeting_days
 
+        # iterate through instructors that can assign to this class
         for instructor in classes[i][1]:
+            # continue when instructor not found in instructors_load
             if instructor not in instructors_load:
                 continue
             else:
                 curr_instructor_classes = curr_solution[instructor]
                 time_conflict = False
+                # check current instructor's classes' time for time conflict
                 for c in curr_instructor_classes:
                     instructor_class = ADTAA_models.Class.objects.get(course_number=c)
                     c_start_time = instructor_class.start_time
@@ -270,19 +284,22 @@ def make_solution(classes, instructors_load, rest_classes, curr_solution):
                         break
                 if time_conflict:
                     continue
+                # if not time conflict, assign instructor to this class, and delete class from rest_of_classes list
                 else:
+                    # decrease instructor's class load
                     instructors_load[instructor] = instructors_load[instructor] - 1
                     curr_solution[instructor].append(classes[i][0])
+                    del rest_of_classes[classes[i][0]]
 
-                    del rest_classes[classes[i][0]]
+                    # if instructor's class load is 0, remove instructor from instructors_load list
                     if instructors_load[instructor] == 0:
                         del instructors_load[instructor]
                         if len(instructors_load) == 0:
                             break
-                    if len(rest_classes) == 0:
+                    if len(rest_of_classes) == 0:
                         break
                     break
-    return len(rest_classes)
+    return len(rest_of_classes)
 
 
 @method_decorator(login_required(), name='get')
@@ -300,83 +317,91 @@ class GenerateSolutions(View):
         if 'create_solution' in request.POST:
             username = request.session.get('username', '0')
             user = ADTAA_models.BaseUser.objects.get(username=username)
+            # get instructors and classes data from databases.
             instructors_list = ADTAA_models.Instructor.objects.all()
             classes_list = ADTAA_models.Class.objects.all()
+            # initialize values for algorithm
             classes = {}
             not_assigned_classes = []
             instructors_load = {}
             temp_solution = {}
             i_list = []
 
+            # add instructors' id_to i_list, add instructors' max class load number to instructors_load,
+            # add instructor_id to temp_solution as key.
             for i in instructors_list:
                 i_list.append(i.instructor_id)
                 instructors_load[i.instructor_id] = int(i.maximum_class_load)
                 temp_solution[i.instructor_id] = []
 
-            biggest_disciplines_area_count = 0
+            # go through classes_list and instructors_list, find instructors and classes which have same disciplines_area
+            # classes is a dictionary which course_numbers are key,
+            # class' disciplines area number and instructors' id who have same disciplines_area are value
             for i in classes_list:
+                biggest_disciplines_area_count = 0
                 classes[i.course_number] = [i.disciplines_area.count()]
                 for j in instructors_list:
                     if any(x in list(i.disciplines_area.all()) for x in list(j.disciplines_area.all())):
+                        # sort instructor id by disciplines area number of the instructor
                         if j.disciplines_area.count() > biggest_disciplines_area_count:
                             classes[i.course_number].insert(1, j.instructor_id)
                             biggest_disciplines_area_count = j.disciplines_area.count()
                         else:
                             classes[i.course_number].append(j.instructor_id)
                     else:
+                        # add classes which has not instructors to not_assigned_classes
                         if j == instructors_list.last() and len(classes[i.course_number]) == 1:
                             not_assigned_classes.append(i.course_number)
                             del classes[i.course_number]
 
-            # one_instructor_classes = {k: v for k, v in classes.items() if len(v) == 2}
-            # classes = {k: v for k, v in classes.items() if len(v) > 2}
-
-            # sort classes by disciplines_area numbers
+            # sort classes by disciplines_area numbers of classes
             classes = {k: v for k, v in sorted(classes.items(), key=lambda item: item[1], reverse=True)}
+            # change classes dict to list
             classes = list(classes.items())
+            # delete disciplines_area numbers in classes after sort
             for c in classes:
                 c[1].pop(0)
-            rest_classes = dict(classes)
-            print("before solution:", classes)
-            # print("one_instructor_classes:", one_instructor_classes)
-            print("not_assigned_classes", not_assigned_classes)
 
+            # initialize values
+            rest_of_classes = dict(classes)
             classes = classes.copy()
             temp_instructors_load = instructors_load.copy()
-            not_assigned_classes_num = make_solution(classes, temp_instructors_load, rest_classes,
+            # generate solution
+            not_assigned_classes_num = make_solution(classes, temp_instructors_load, rest_of_classes,
                                                      temp_solution)
 
-            print(rest_classes)
-            print(temp_solution)
+            # assign values
+            best_solution = temp_solution.copy()
+            lowest_not_assigned_classes = rest_of_classes
 
-            if len(rest_classes) == 0:
+            # if rest_of_classes is not empty, which means there are some classes are not assigned
+            # randomly shuffle classes list and generate a new solution
+            # repeat many times until found best solution
+            # if best solution is not exist, then the best solution is the solution has the lowest not assigned class
+            if len(rest_of_classes) != 0:
                 best_solution = temp_solution.copy()
-                lowest_not_assigned_classes = rest_classes
-            else:
-                best_solution = temp_solution.copy()
-                lowest_not_assigned_classes = rest_classes
+                lowest_not_assigned_classes = rest_of_classes
                 for x in range(0, 50):
                     temp_classes = random.sample(classes, len(classes))
                     for i in temp_classes:
                         random.shuffle(i[1])
                     temp_instructors_load = instructors_load.copy()
-                    rest_classes = dict(temp_classes)
+                    rest_of_classes = dict(temp_classes)
                     for i in instructors_list:
                         temp_solution[i.instructor_id] = []
-                    print('-----------')
-                    print(temp_classes)
-                    new_not_assigned_classes_num = make_solution(temp_classes, temp_instructors_load, rest_classes,
+
+                    new_not_assigned_classes_num = make_solution(temp_classes, temp_instructors_load, rest_of_classes,
                                                                  temp_solution)
-                    print(temp_solution)
-                    print(rest_classes)
+
                     if new_not_assigned_classes_num < not_assigned_classes_num:
                         not_assigned_classes_num = new_not_assigned_classes_num
-                        lowest_not_assigned_classes = rest_classes
+                        lowest_not_assigned_classes = rest_of_classes
                         best_solution = temp_solution.copy()
-                        print("!")
-                    if len(rest_classes) == 0:
+
+                    if len(rest_of_classes) == 0:
                         break
 
+            # make a solution dict for html context
             solution_context = []
             for instructor, classes in best_solution.items():
                 i = ADTAA_models.Instructor.objects.get(instructor_id=instructor)
@@ -389,8 +414,8 @@ class GenerateSolutions(View):
                                              'End_time': instructor_class.end_time,
                                              'Instructor': i.last_name,
                                              'Instructor_id': i.instructor_id})
-            print(solution_context)
 
+            # make a not_assigned_classes dict for html context
             for i in not_assigned_classes:
                 lowest_not_assigned_classes[i] = ''
             not_assigned_classes_context = []
@@ -402,6 +427,7 @@ class GenerateSolutions(View):
                                                      'Start_time': not_assigned_class.start_time,
                                                      'End_time': not_assigned_class.end_time,
                                                      })
+            # save solution in session
             request.session['solution'] = best_solution
             context = {
                 'solution': solution_context,
@@ -421,6 +447,7 @@ class GenerateSolutions(View):
         if 'save' in request.POST:
             username = request.session.get('username', '0')
             user = ADTAA_models.BaseUser.objects.get(username=username)
+            # save solution into database
             best_solution = request.session.get('solution', None)
             if best_solution:
                 for instructor, classes in best_solution.items():
@@ -439,6 +466,7 @@ class GenerateSolutions(View):
         if 'reset' in request.POST:
             username = request.session.get('username', '0')
             user = ADTAA_models.BaseUser.objects.get(username=username)
+            # reset solution in database
             classes_list = ADTAA_models.Class.objects.all()
             for c in classes_list:
                 c.assigned_instructor = "No Instructor"
@@ -546,6 +574,7 @@ class ChangePassword(View):
         self.password_form.save()
         messages.success(request, 'Your Password Has Been Changed.')
         return redirect('/ADTAA')
+
     # check username of user requesting to change password to make sure that the user is a registered user
     # in the database
     @staticmethod
